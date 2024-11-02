4f4561b0d2de81dc625fd84bb6efc076e0d3c1a0
zlitjr <amanbekovaaruzhan785@gmail.com>
2024-11-03 01:59:22 +0500

    -seasonal

--- patterns/chainofresponsibility/SeasonalDiscountHandler.java
+++ patterns/chainofresponsibility/SeasonalDiscountHandler.java
@@ -7,7 +7,7 @@ public class SeasonalDiscountHandler extends DiscountHandler {
     @Override
     public double applyDiscount(Car car, double price) {
         if (car.isSeasonalDiscountAvailable()) {
-            System.out.println("Applying seasonal discount");
+            System.out.println("Applying discount");
             price *= 0.85;  // 15% скидка
         }
         return next != null ? next.applyDiscount(car, price) : price;
